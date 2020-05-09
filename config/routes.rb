9f4ad030b651475f3b3976daa8e0532f2e2aa07f
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-09 13:39:09 +0100

    Update routes and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   resources :docs
 end
